9aefb6e2bb7dae0979bb7a062e579f4697fbd962
None <None>
2017-11-04 18:36:47 -0700

    first commit

---
+++ fibre-2a-opencall-sdn-ids.py
@@ -0,0 +1,386 @@
+# Copyright (C) Italo Valcy S Brito <italovalcy@ufba.br>
+#               Adriana V Ribeiro <adrianavr@dcc.ufba.br>
+#
+# Simple application that runs BGP, packet mirror to IDS and REST 
+# API for an IPS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event,dpset
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3,ofproto_v1_0
+from ryu.lib import dpid as dpid_lib
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+from ryu.lib.packet import arp
+from ryu.ofproto import ether
+from ryu.topology import event, switches
+from ryu.topology.api import get_switch, get_link
+from ryu.lib import mac
+from ryu.app.wsgi import ControllerBase, WSGIApplication, route
+from ryu.services.protocols.bgp.bgpspeaker import BGPSpeaker
+from webob import Response
+import networkx as nx
+import json
+import socket, os
+
+myapp_name = 'sdnips_app'
+base_url = '/sdnips'
+
+REST_RESULT = 'result'
+REST_DETAILS = 'details'
+REST_OK = 'success'
+REST_NG = 'failure'
+
+class SDNIPSApp(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+    _CONTEXTS = {'wsgi': WSGIApplication}
+
+    def __init__(self, *args, **kwargs):
+        super(SDNIPSApp, self).__init__(*args, **kwargs)
+        self.net = nx.DiGraph()
+        wsgi = kwargs['wsgi']
+        wsgi.register(SDNIPSWSGIApp,{myapp_name: self})
+        self.bgp_speaker = None
+        self.prefixes = []
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        for prefix in self.prefixes:
+            os.system('/sbin/ip route del %s' % prefix)
+
+    @set_ev_cls(dpset.EventDP, dpset.DPSET_EV_DISPATCHER)
+    def datapath_handler(self, ev):
+        ofproto = ev.dp.ofproto
+        if ev.enter:
+            ports = {}
+            for p in ev.ports:
+                if p.port_no != ofproto.OFPP_LOCAL:
+                    ports[p.port_no] = {'name' : p.name, 'hw_addr' : p.hw_addr}
+            self.net.add_node(ev.dp.id, **{'ports': ports, 'conn': ev.dp})
+            print 'OFPStateChange switch entered: datapath_id=0x%016x ports=%s' % (ev.dp.id, ev.ports)
+        else:
+            print 'OFPStateChange switch leaves: datapath_id=0x%016x' % (ev.dp.id)
+            self.net.remove_node(ev.dp.id)
+
+    topo_events = [event.EventSwitchEnter, event.EventPortAdd, event.EventLinkAdd]
+    @set_ev_cls(topo_events)
+    def get_topology_data(self, ev):
+        switch_list = get_switch(self, None)
+        links_list = get_link(self, None)
+        links = []
+        for link in links_list:
+            # check for unwanted nodes and links
+            if link.src.dpid not in self.net.nodes() or link.dst.dpid not in self.net.nodes():
+                next
+            links.append((link.src.dpid,link.dst.dpid,{'sport':link.src.port_no, 'dport':link.dst.port_no}))
+        print "Update graph edges: %s" % (links)
+        self.net.add_edges_from(links)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # invalidate all previews rules
+        clear = parser.OFPFlowMod(datapath=datapath, command=ofproto.OFPFC_DELETE)
+        datapath.send_msg(clear)
+
+        # install table-miss flow entry
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        self.add_flow(datapath, 65530, match, actions)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        dpid = msg.datapath.id
+        pkt = packet.Packet(msg.data)
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+
+        if eth_pkt.ethertype == ether_types.ETH_TYPE_LLDP:
+            return
+
+        print "PacketIn dpid=%s inport=%s src=%s dst=%s ethertype=0x%04x" % \
+                (dpid, msg.in_port, eth_pkt.src, eth_pkt.dst, eth_pkt.ethertype)
+
+    def get_access_ports(self, sw):
+        if sw not in self.net.nodes():
+            return None
+        ports = self.net.node[sw]['ports'].keys()
+        for e in self.net.edge[sw]:
+            ports.remove(self.net.edge[sw][e]['sport'])
+        return ports
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, actions=actions)
+        datapath.send_msg(mod)
+
+    def build_match(self, datapath, in_port=0, dl_type=0, dl_src=0, dl_dst=0, 
+                 dl_vlan=0,nw_src=0, src_mask=32, nw_dst=0, dst_mask=32,
+                 nw_proto=0):
+        ofp = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+        
+        # Match
+        wildcards = ofp.OFPFW_ALL
+        if in_port:
+            wildcards &= ~ofp.OFPFW_IN_PORT
+        if dl_type:
+            wildcards &= ~ofp.OFPFW_DL_TYPE
+        if dl_src:
+            wildcards &= ~ofp.OFPFW_DL_SRC
+        if dl_dst:
+            wildcards &= ~ofp.OFPFW_DL_DST
+        if dl_vlan:
+            wildcards &= ~ofp.OFPFW_DL_VLAN
+        if nw_src:
+            v = (32 - src_mask) << ofp.OFPFW_NW_SRC_SHIFT | \
+                ~ofp.OFPFW_NW_SRC_MASK
+            wildcards &= v
+        if nw_dst:
+            v = (32 - dst_mask) << ofp.OFPFW_NW_DST_SHIFT | \
+                ~ofp.OFPFW_NW_DST_MASK
+            wildcards &= v
+        if nw_proto:
+            wildcards &= ~ofp.OFPFW_NW_PROTO
+
+        match = ofp_parser.OFPMatch(wildcards, in_port, dl_src, dl_dst, dl_vlan, 0,
+                                    dl_type, 0, nw_proto,
+                                    nw_src, nw_dst, 0, 0, src_mask, dst_mask)
+        return match
+
+    def create_eline(self, uniA_sw, uniA_port, uniB_sw, uniB_port, vlanid):
+        path = nx.shortest_path(self.net, uniA_sw, uniB_sw)
+        print "==> create_eline(UNI-A=%s:%s, UNI-B=%s:%s, vlanid=%d): %s" % \
+                (uniA_sw, uniA_port, uniB_sw, uniB_port, vlanid, path)
+        for i in range(len(path)):
+            sw = path[i]
+            buff_id = None
+            if i == 0: # first switch
+                match_in_port = uniA_port
+            else:
+                prev_sw = path[i-1]
+                match_in_port = self.net.edge[prev_sw][sw]['dport']
+            if i == len(path)-1:
+                action_out_port = uniB_port
+            else:
+                next_sw = path[i+1]
+                action_out_port = self.net.edge[sw][next_sw]['sport']
+            dp = self.net.node[sw]['conn']
+            # uniA -> uniB
+            self.logger.info("==> add_flow sw=%s (->) in_port=%s vlanid=%d action_out_port=%s", sw, match_in_port, vlanid, action_out_port)
+            match = self.build_match(dp, in_port=match_in_port, dl_vlan=vlanid)
+            actions = [dp.ofproto_parser.OFPActionOutput(action_out_port)]
+            self.add_flow(dp, 65535, match, actions)
+            # uniB -> uniA
+            match_in_port, action_out_port = action_out_port, match_in_port
+            self.logger.info("==> add_flow sw=%s (<-) in_port=%s vlanid=%d action_out_port=%s", sw, match_in_port, vlanid, action_out_port)
+            match = self.build_match(dp, in_port=match_in_port, dl_vlan=vlanid)
+            actions = [dp.ofproto_parser.OFPActionOutput(action_out_port)]
+            self.add_flow(dp, 65535, match, actions)
+
+        return (True, 'success')
+
+    def bgp_create(self, as_number, router_id):
+        try:
+            self.bgp_speaker = BGPSpeaker(as_number=as_number, router_id=router_id,
+                            best_path_change_handler=self.best_path_change_handler,
+                            peer_down_handler=self.peer_down_handler,
+                            peer_up_handler=self.peer_up_handler)
+        except:
+            return (False, 'Failed to create BGP speaker')
+
+        return (True, 'Success')
+
+    def bgp_add_neighbor(self, address, remote_as):
+        try:
+            self.bgp_speaker.neighbor_add(address, remote_as)
+        except:
+            return (False, 'Failed to add BGP neighbor')
+        return (True, 'Success')
+
+    def bgp_add_prefix(self, prefix):
+        try:
+            self.bgp_speaker.prefix_add(prefix)
+        except:
+            return (False, 'Failed to add prefix')
+        return (True, 'Success')
+
+    def best_path_change_handler(self, event):
+        action = 'del' if event.is_withdraw else 'add'
+        print 'the best path changed: remote-as=%s prefix=%s next-hop=%s action=%s' % \
+                (event.remote_as, event.prefix, event.nexthop, action)
+        os.system('/sbin/sysctl net.ipv4.ip_forward=1')
+        os.system('/sbin/ip route %s %s via %s' % (action, event.prefix, event.nexthop))
+        if event.is_withdraw:
+            try:
+                self.prefixes.remove(event.prefix)
+            except:
+                pass
+        else:
+            self.prefixes.append(event.prefix)
+    
+    def peer_down_handler(self, remote_ip, remote_as):
+        print 'Peer down:', remote_ip, remote_as
+
+    def peer_up_handler(self, remote_ip, remote_as):
+        print 'Peer up:', remote_ip, remote_as
+
+
+class SDNIPSWSGIApp(ControllerBase):
+    def __init__(self, req, link, data, **config):
+        super(SDNIPSWSGIApp, self).__init__(req, link, data, **config)
+        self.myapp = data[myapp_name]
+
+    @route(myapp_name, base_url + '/switches', methods=['GET'])
+    def list_switches(self, req, **kwargs):
+        body = json.dumps(map(dpid_lib.dpid_to_str, self.myapp.net.nodes()))
+        return Response(content_type='application/json', body=body)
+
+    @route(myapp_name, base_url + '/switches/{dpid}/ports', methods=['GET'])
+    def get_switch_ports(self, req, **kwargs):
+        dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
+        if dpid not in self.myapp.net.nodes():
+            details = 'switch not found - invalid dpid'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=404, body=json.dumps(msg))
+
+        body = json.dumps(self.myapp.get_access_ports(dpid))
+        return Response(content_type='application/json', body=body)
+
+    @route(myapp_name, base_url + '/e-line/create', methods=['POST'])
+    def eline_create(self, req, **kwargs):
+        try:
+            eline_params = req.json if req.body else {}
+        except ValueError:
+            details = 'Missing E-Line parameters - uniA, uniB, vlanid'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        try:
+            uniA_sw, uniA_port = eline_params['uniA'].split(':')
+            uniA_sw = dpid_lib.str_to_dpid(uniA_sw)
+            assert uniA_sw in self.myapp.net.nodes()
+            uniA_port = int(uniA_port)
+            assert uniA_port in self.myapp.get_access_ports(uniA_sw)
+        except:
+            details = 'Invalid UNI A parameter. Format: uniA : "dpid:port"'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        try:
+            uniB_sw, uniB_port = eline_params['uniB'].split(':')
+            uniB_sw = dpid_lib.str_to_dpid(uniB_sw)
+            assert uniB_sw in self.myapp.net.nodes()
+            uniB_port = int(uniB_port)
+            assert uniB_port in self.myapp.get_access_ports(uniB_sw)
+        except:
+            details = 'Invalid UNI B parameter. Format: uniB : "dpid:port"'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        try:
+            vlanid = int(eline_params['vlanid'])
+            assert 0 < vlanid < 4096
+        except:
+            details = 'Invalid VLAN ID parameter.'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        status, msg = self.myapp.create_eline(uniA_sw, uniA_port, uniB_sw, uniB_port, vlanid)
+
+        body = json.dumps([msg])
+        return Response(content_type='application/json', body=body)
+
+    @route(myapp_name, base_url + '/bgp/create', methods=['POST'])
+    def bgp_create(self, req, **kwargs):
+        try:
+            bgp_params = req.json if req.body else {}
+        except ValueError:
+            details = 'Missing BGP parameters - as_number, router_id'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        try:
+            as_number = bgp_params['as_number']
+            assert isinstance(as_number, int)
+        except:
+            details = 'Invalid as_number parameter'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        try:
+            router_id = str(bgp_params['router_id'])
+            socket.inet_aton(router_id)
+        except:
+            details = 'Invalid router_id parameter'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        status, msg = self.myapp.bgp_create(as_number, router_id)
+
+        body = json.dumps([msg])
+        return Response(content_type='application/json', body=body)
+
+    @route(myapp_name, base_url + '/bgp/add_neighbor', methods=['POST'])
+    def bgp_add_neighbor(self, req, **kwargs):
+        try:
+            bgp_params = req.json if req.body else {}
+        except ValueError:
+            details = 'Missing BGP parameters - remote_as, address'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        try:
+            address = str(bgp_params['address'])
+            socket.inet_aton(address)
+        except:
+            details = 'Invalid address parameter'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        try:
+            remote_as = bgp_params['remote_as']
+            assert isinstance(remote_as, int)
+        except:
+            details = 'Invalid remote_as parameter'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        status, msg = self.myapp.bgp_add_neighbor(address, remote_as)
+
+        body = json.dumps([msg])
+        return Response(content_type='application/json', body=body)
+
+    @route(myapp_name, base_url + '/bgp/add_prefix', methods=['POST'])
+    def bgp_add_prefix(self, req, **kwargs):
+        try:
+            prefix = str(req.json['prefix'])
+        except:
+            details = 'Invalid address parameter'
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: details}
+            return Response(status=400, body=json.dumps(msg))
+
+        status, msg = self.myapp.bgp_add_prefix(prefix)
+
+        body = json.dumps([msg])
+        return Response(content_type='application/json', body=body)
